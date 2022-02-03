ca188353ef38f90b90d80246e4da2221ccfbeec8
Quanzat <72586011+Quanzat@users.noreply.github.com>
2022-02-03 15:23:52 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,18 +6,43 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParseTest {
-
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-    
+
     @Test
-    public void testGetlink() throws IOException{
-        Path testFile_1 = Path.of("test-file.md");
-        ArrayList<String> test1 = new ArrayList<String>();
-        test1.add("https://something.com");
-        test1.add("some-page.html");       
-        assertEquals(test1, MarkdownParse.getLinks(Files.readString(testFile_1)));
+    public void getLinksTest() throws IOException {
+        MarkdownParse m = new MarkdownParse();
+        Path filename = Path.of("test-file.md");
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-page.html");
+        assertEquals(expected, m.getLinks(Files.readString(filename)));
+
+    }
+
+    @Test
+    public void getLinkTest1() throws IOException {
+        MarkdownParse m = new MarkdownParse();
+        Path filename = Path.of("test1.md");
+        ArrayList<String> expected = new ArrayList<String>();
+        assertEquals(expected, m.getLinks(Files.readString(filename)));
+    }
+
+    @Test
+    public void getLinkTest2() throws IOException {
+        MarkdownParse m = new MarkdownParse();
+        Path filename = Path.of("test2.md");
+        ArrayList<String> expected = new ArrayList<String>();
+        assertEquals(expected, m.getLinks(Files.readString(filename)));
+    }
+
+    @Test
+    public void getLinkTest3() throws IOException {
+        MarkdownParse m = new MarkdownParse();
+        Path filename = Path.of("test3.md");
+        ArrayList<String> expected = new ArrayList<String>();
+        assertEquals(expected, m.getLinks(Files.readString(filename)));
     }
 }
